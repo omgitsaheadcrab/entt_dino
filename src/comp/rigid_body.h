63fd18b1dc448fe11b2a17720648eefb768a6d79
Tobias Backer Dirks <omgitsaheadcrab@gmail.com>
2021-06-08 19:30:47 +0100

    feat: add rigid body component

---
+++ src/comp/rigid_body.h
@@ -0,0 +1,19 @@
+/**
+ * @file      rigid_body.h
+ * @brief     Rigid body component
+ * @author    Tobias Backer Dirks <omgitsaheadcrab[at]gmail.com>
+ * @date      2021-06-08
+ * @copyright Copyright © 2021 Tobias Backer Dirks
+ */
+
+#ifndef ENTT_DINO_SRC_COMP_RIGID_BODY_H_
+#define ENTT_DINO_SRC_COMP_RIGID_BODY_H_
+
+#include "core/vec2d.h"
+
+struct RigidBody {
+  vf2d velocity;
+  vf2d acceleration;
+};
+
+#endif /* ENTT_DINO_SRC_COMP_RIGID_BODY_H_ */
