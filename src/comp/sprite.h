4d510ab39766db900eba78b70cc0768eaae5da44
Tobias Backer Dirks <omgitsaheadcrab@gmail.com>
2021-06-08 19:44:11 +0100

    feat: add sprite component

---
+++ src/comp/sprite.h
@@ -0,0 +1,22 @@
+/**
+ * @file      sprite.h
+ * @brief     Sprite component
+ * @author    Tobias Backer Dirks <omgitsaheadcrab[at]gmail.com>
+ * @date      2021-06-08
+ * @copyright Copyright © 2021 Tobias Backer Dirks
+ */
+
+#ifndef ENTT_DINO_SRC_COMP_SPRITE_H_
+#define ENTT_DINO_SRC_COMP_SPRITE_H_
+
+#include <SDL2/SDL_image.h>
+
+#include "core/vec2d.h"
+
+struct Sprite {
+  SDL_Texture* sheet;
+  vf2d position;
+  vf2d size;
+};
+
+#endif /* ENTT_DINO_SRC_COMP_SPRITE_H_ */
