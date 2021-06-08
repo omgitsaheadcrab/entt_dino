6fd7c5619bc717668cee16957040659eb463e43a
Tobias Backer Dirks <omgitsaheadcrab@gmail.com>
2021-06-08 19:28:40 +0100

    feat: add gravity component

---
+++ src/comp/gravity.h
@@ -0,0 +1,18 @@
+/**
+ * @file      gravity.h
+ * @brief     Gravity component
+ * @author    Tobias Backer Dirks <omgitsaheadcrab[at]gmail.com>
+ * @date      2021-06-08
+ * @copyright Copyright © 2021 Tobias Backer Dirks
+ */
+
+#ifndef ENTT_DINO_SRC_COMP_GRAVITY_H_
+#define ENTT_DINO_SRC_COMP_GRAVITY_H_
+
+#include "core/vec2d.h"
+
+struct Gravity {
+  vf2d force;
+};
+
+#endif /* ENTT_DINO_SRC_COMP_GRAVITY_H_ */
