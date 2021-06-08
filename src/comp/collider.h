5671382d560dce2a4206f4b8daf1bcaf1dd6c363
Tobias Backer Dirks <omgitsaheadcrab@gmail.com>
2021-06-08 19:45:23 +0100

    feat: add collider component

---
+++ src/comp/collider.h
@@ -0,0 +1,18 @@
+/**
+ * @file      collider.h
+ * @brief     Collider component
+ * @author    Tobias Backer Dirks <omgitsaheadcrab[at]gmail.com>
+ * @date      2021-06-08
+ * @copyright Copyright © 2021 Tobias Backer Dirks
+ */
+
+#ifndef ENTT_DINO_SRC_COMP_COLLIDER_H_
+#define ENTT_DINO_SRC_COMP_COLLIDER_H_
+
+#include "core/vec2d.h"
+
+struct Collider {
+  vf2d size;
+};
+
+#endif /* ENTT_DINO_SRC_COMP_COLLIDER_H_ */
