e169e03aadd62f55faafeb8a7c4658a478a6b8f7
Tobias Backer Dirks <omgitsaheadcrab@gmail.com>
2021-06-13 23:58:00 +0100

    chore: update dir-locals

--- .dir-locals.el
+++ .dir-locals.el
@@ -1,5 +1,4 @@
 ;;; Directory Local Variables
 ;;; For more information see (info "(emacs) Directory Variables")
 
-((c++-mode . ((projectile-project-run-cmd . "./bin/entt_dino")
-              (projectile-project-compilation-cmd . "cmake -H. -Bbuild -DCMAKE_BUILD_TYPE=Debug -DCMAKE_EXPORT_COMPILE_COMMANDS=YES"))))
+((c++-mode . ((projectile-project-run-cmd . "./bin/entt_dino"))))
