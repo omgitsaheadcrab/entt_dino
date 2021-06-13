4fa2d3b245792139d177b2acfdea1900d9698d2a
Tobias Backer Dirks <omgitsaheadcrab@gmail.com>
2021-06-13 19:07:41 +0100

    chore: add emacs dir-locals for project

---
+++ .dir-locals.el
@@ -0,0 +1,5 @@
+;;; Directory Local Variables
+;;; For more information see (info "(emacs) Directory Variables")
+
+((c++-mode . ((projectile-project-run-cmd . "./bin/entt_dino")
+              (projectile-project-compilation-cmd . "cmake -H. -Bbuild -DCMAKE_BUILD_TYPE=Debug -DCMAKE_EXPORT_COMPILE_COMMANDS=YES"))))
