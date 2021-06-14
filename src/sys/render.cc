6692972f3c221cc8903998fff9efcf02097ecc7f
Tobias Backer Dirks <omgitsaheadcrab@gmail.com>
2021-06-14 12:49:20 +0100

    feat: add Render system

---
+++ src/sys/render.cc
@@ -0,0 +1,24 @@
+/**
+ * @file      render.cc
+ * @brief     Render system
+ * @author    Tobias Backer Dirks <omgitsaheadcrab[at]gmail.com>
+ * @date      2021-06-13
+ * @copyright Copyright © 2021 Tobias Backer Dirks
+ */
+
+#include "sys/render.h"
+
+#include <SDL2/SDL_render.h>
+#include <SDL_rect.h>
+
+#include <entt/entity/registry.hpp>
+
+#include "comp/sprite.h"
+#include "comp/transform.h"
+
+void systems::Render(SDL_Renderer* renderer, entt::registry* registry) {
+  const auto view = registry->view<components::Sprite, components::Transform>();
+  view.each([&](const auto& sprite, const auto& transform) {
+    SDL_RenderCopy(renderer, sprite.sheet, sprite.clip, transform.position);
+  });
+}
---
+++ src/sys/render.h
@@ -0,0 +1,22 @@
+/**
+ * @file      render.h
+ * @brief     Render system
+ * @author    Tobias Backer Dirks <omgitsaheadcrab[at]gmail.com>
+ * @date      2021-06-13
+ * @copyright Copyright © 2021 Tobias Backer Dirks
+ */
+
+#ifndef ENTT_DINO_SRC_SYS_RENDER_H_
+#define ENTT_DINO_SRC_SYS_RENDER_H_
+
+#include <SDL2/SDL_render.h>
+
+#include <entt/entity/registry.hpp>
+
+namespace systems {
+
+void Render(SDL_Renderer *renderer, entt::registry *registry);
+
+}  // namespace systems
+
+#endif  // ENTT_DINO_SRC_SYS_RENDER_H_
