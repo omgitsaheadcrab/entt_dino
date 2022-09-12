5d8c02cba881db1fb425ce7ead5fa33b3a83004c
Tobias Backer Dirks <omgitsaheadcrab@gmail.com>
2022-09-12 15:17:12 +0300

    chore: fix header guard

--- src/comp/entity_states/action.h
+++ src/comp/entity_states/action.h
@@ -1,13 +1,13 @@
 /**
- * @file      actions.h
+ * @file      action.h
  * @brief     Entity action states
  * @author    Tobias Backer Dirks <omgitsaheadcrab[at]gmail.com>
  * @date      2022-09-09
  * @copyright Copyright © 2022 Tobias Backer Dirks
  */
 
-#ifndef ENTT_DINO_SRC_COMP_ENTITY_STATES_ACTIONS_H_
-#define ENTT_DINO_SRC_COMP_ENTITY_STATES_ACTIONS_H_
+#ifndef ENTT_DINO_SRC_COMP_ENTITY_STATES_ACTION_H_
+#define ENTT_DINO_SRC_COMP_ENTITY_STATES_ACTION_H_
 
 #include <cstdint>
 
@@ -25,4 +25,4 @@ struct Action {
 
 }  // namespace components
 
-#endif  // ENTT_DINO_SRC_COMP_ENTITY_STATES_ACTIONS_H_
+#endif  // ENTT_DINO_SRC_COMP_ENTITY_STATES_ACTION_H_
